08de2e6926a85f6d3deaccd6b8f0af6711b5360a
Graham Bing <grrraham@gmail.com>
2016-03-30 03:57:36 -0700

    extend User to work with queries

--- src/cpsc304nsa/tables/User.java
+++ src/cpsc304nsa/tables/User.java
@@ -1,6 +1,12 @@
 package cpsc304nsa.tables;
 
-public class User {
+import java.util.Arrays;
+import java.util.List;
+
+import cpsc304nsa.main.AttrType;
+import cpsc304nsa.main.Pair;
+
+public class User implements Table {
 
 	private int userId;
 
@@ -10,5 +16,15 @@ public class User {
 
 	public void setUserId(int userId) {
 		this.userId = userId;
+	}
+
+	@Override
+	public List<Pair<AttrType, String>> getAttrs() {
+		return Arrays.asList(new Pair(AttrType.INT, "user_id"));
+	}
+
+	@Override
+	public String getName() {
+		return "user";
 	}
 }
