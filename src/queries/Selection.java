d8a00b262a5cd7e3851c5f24da1e2bb4342cf13a
Graham Bing <grrraham@gmail.com>
2016-03-29 20:07:39 -0700

    Selection class stub

---
+++ src/queries/Selection.java
@@ -0,0 +1,33 @@
+package queries;
+
+import java.io.IOException;
+
+import cpsc304nsa.tables.Device;
+
+public class Selection {
+		
+	public Selection() {
+
+	}
+	
+	public void select() {
+		selectTable();
+
+	}
+	private void selectTable() {
+//		System.out.println("Enter the DeviceID:");
+//        
+//        int deviceId;
+//        try {
+//            deviceId = Integer.parseInt(br.readLine());
+//            Device device = getDevice(deviceId);
+//            System.out.println(device);           
+//        } catch (NumberFormatException nfe) {
+//            nfe.printStackTrace();
+//        } catch (IOException ioe) {
+//            ioe.printStackTrace();
+//        }
+
+	}
+	
+}
