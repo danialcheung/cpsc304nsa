6e1510f5d4f57b7a990cefbd060971d41427dc4e
Graham Bing <grrraham@gmail.com>
2016-04-01 16:25:45 -0700

    countSuspiciousDataByCountry

--- src/queries/Aggregation.java
+++ src/queries/Aggregation.java
@@ -1,7 +1,12 @@
 package queries;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import main.AttrType;
 import main.Pair;
@@ -13,7 +18,6 @@ public class Aggregation extends Query {
 	public Aggregation(Connection con) {
 		super(con);
 		// (1 point) Aggregation query: pick one query that requires the use of aggregation (min, max, average, or count are all fine).
-
 	}
 	
 	public void aggregate() {
@@ -35,5 +39,26 @@ public class Aggregation extends Query {
 		String query = "SELECT " + aggrType + "(" + aggrAttr.getRight() + ") FROM " + table.getName() + ";";
 		runQuery(query, Arrays.asList(new Pair<AttrType, String>(aggrAttr.getLeft(), aggrType + "(" + aggrAttr.getRight() + ")")));
 	}
+	
+	public List<Pair<String,Integer>> countSuspiciousDataByCountry() {
+		List<Pair<String,Integer>> countryCount = new ArrayList<Pair<String,Integer>>();
+		String query = "SELECT country, COUNT(data_id)\n"
+				+ "FROM data, location\n"
+				+ "WHERE data.lat = location.lat AND data.lng = location.lng AND suspicious = 1\n"
+				+ "GROUP BY country;";
+		
+        ResultSet rs = null;
+        Statement statement = null; 
+        try {           
+            statement = con.createStatement();
+            rs = statement.executeQuery(query);
+            while (rs.next()) {
+            	countryCount.add(new Pair<String,Integer>(rs.getString("country"),rs.getInt("COUNT(data_id)")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
+		return countryCount;	
+	}
 }
