77f6eebb617f27a364520e5d847fe4793aac107e
Graham Bing <grrraham@gmail.com>
2016-04-01 16:31:59 -0700

    markDataSuspcious

--- src/queries/Update.java
+++ src/queries/Update.java
@@ -1,17 +1,33 @@
 package queries;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Update extends Query {
 
 	public Update(Connection con) {
 		super(con);
-		// TODO Auto-generated constructor stub
+		markDataSuspicious(7712323);
 	}
 
 	public void update() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	/* mark data as suspicious */
+	public void markDataSuspicious(Integer data_id) {
+		String update = "UPDATE data SET suspicious = 1 WHERE data_id = " + data_id + ";";
+		
+        Statement statement = null; 
+        try {           
+            statement = con.createStatement();
+            statement.executeUpdate(update);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 	}
 
 }
