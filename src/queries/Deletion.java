36a40bb04e9e796c044fbbd945531c06c4a65047
Graham Bing <grrraham@gmail.com>
2016-04-01 16:50:29 -0700

    deleteDevice (and all associated data)

--- src/queries/Deletion.java
+++ src/queries/Deletion.java
@@ -1,6 +1,8 @@
 package queries;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import main.AttrType;
 import main.Pair;
@@ -10,6 +12,7 @@ public class Deletion extends Query {
 
 	public Deletion(Connection con) {
 		super(con);
+		deleteDevice(8823);
 	}
 
 	public void delete() {
@@ -17,6 +20,23 @@ public class Deletion extends Query {
 		String attr = table.primaryKey();
 		
 		String val = enterInput("enter primary key of entry to delete");
+	}
+	
+	/* remove all records of a device from database */
+	public void deleteDevice(Integer device_id) {
+        Statement statement = null; 
+        try {           
+            statement = con.createStatement();
+            statement.executeUpdate("DELETE FROM `call` WHERE EXISTS (SELECT * FROM data WHERE data.device_id = " + device_id + " AND data.data_id = `call`.data_id);");
+            statement.executeUpdate("DELETE FROM text WHERE EXISTS (SELECT * FROM data WHERE data.device_id = " + device_id + " AND data.data_id = text.data_id);");
+            statement.executeUpdate("DELETE FROM commlog WHERE EXISTS (SELECT * FROM data WHERE data.device_id = " + device_id + " AND data.data_id = commlog.data_id);");
+            statement.executeUpdate("DELETE FROM photo WHERE EXISTS (SELECT * FROM data WHERE data.device_id = " + device_id + " AND data.data_id = photo.data_id);");
+            statement.executeUpdate("DELETE FROM transaction WHERE EXISTS (SELECT * FROM data WHERE data.device_id = " + device_id + " AND data.data_id = transaction.data_id);");
+            statement.executeUpdate("DELETE FROM data WHERE device_id = " + device_id + ";");
+            statement.executeUpdate("DELETE FROM device WHERE device_id = " + device_id + ";");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 	}
 
 }
