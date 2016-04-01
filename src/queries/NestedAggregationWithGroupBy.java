5b1d0ba740c67e7dfe4c3e7f89c369528641ccdd
Graham Bing <grrraham@gmail.com>
2016-03-31 18:06:56 -0700

    nested aggregation

--- src/queries/NestedAggregationWithGroupBy.java
+++ src/queries/NestedAggregationWithGroupBy.java
@@ -9,7 +9,6 @@ import tables.Table;
 
 public class NestedAggregationWithGroupBy extends Query {
 	
-	
 	public NestedAggregationWithGroupBy(Connection con) {
 		super(con);
 		// TODO Auto-generated constructor stub
@@ -21,19 +20,25 @@ public class NestedAggregationWithGroupBy extends Query {
 		Table table = selectTable();
 
 		Pair<AttrType, String> groupAttr = selectAttr(table, "select attribute to group by:");
-		Pair<AttrType, String> avgAttr = selectAttr(table, "select attribute to average by:");
+		Pair<AttrType, String> avgAttr = selectAttr(table, "select attribute to average:");
 
+		if (avgAttr.getLeft() == AttrType.STRING) {
+			System.out.println("cannot average string data, query failed");
+			return;
+		}
+		
 		int aggrTypeInt = selectGeneric("aggregation by min or max:", Arrays.asList("min", "max"));
-		String aggrType;
+		String aggrType = "";
 		switch (aggrTypeInt) {
 		case 1: aggrType = "MIN"; break;
 		case 2: aggrType = "MAX"; break;
 		default:
 			System.out.println("invalid input");
 		}
-
-
-//		runQuery( ... );
+		
+		// TODO use aggrType
+		String query = "SELECT AVG(" + avgAttr.getRight() + ") FROM " + table.getName() + " GROUP BY " + groupAttr.getRight() + ";";
+		runQuery(query, Arrays.asList(new Pair<AttrType, String>(avgAttr.getLeft(), "AVG(" + avgAttr.getRight() + ")")));
 	}
 
 }
